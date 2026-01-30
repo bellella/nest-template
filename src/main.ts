2076f23504fa015936c21fc491093a645cd80d4f
Mina <bellella13@gmail.com>
2026-01-30 14:50:42 +0900

    add cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -53,6 +54,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  // enable cookie
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
